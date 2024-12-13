89d95d2034117e14c2743ce0e4b60f44cdd511aa
dethmet <mpgdethmet48@gmail.com>
2024-12-13 10:45:06 +0300

    personal site: main page and link

--- namepage/main/views.py
+++ namepage/main/views.py
@@ -1,17 +1,18 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
 
 def index(request):
-    return HttpResponse('<h1>Проверка работы</h1>')
+    data = {
+        'title': 'Самая главная страница!!',
+        'values': ['Что-то', 'Еще что-то', 'циферки 123'],
+        'obj': {
+            'car': 'Жига',
+            'age': 30,
+            'hobby': 'DotA 2'
+        }
+    }
+    return render(request, 'main/index.html', data)
 
 
 def about(request):
-    return HttpResponse('<h3>Страница для нас</h3>')
-
-
-def huyout(request):
-    return HttpResponse('<h2>Хуевая страница не для нас</h2>')
-
-
-def ebout(request):
-    return HttpResponse('<h4>Ебаная страница для когото</h4>')
+    return render(request, 'main/about.html')
